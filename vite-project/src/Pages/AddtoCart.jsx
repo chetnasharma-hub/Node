74fd1d69ef359d08f2db42e614229641b9dcbb87
chetnasharma-hub <chetnasharma162004@gmail.com>
2025-12-16 14:49:12 +0530

    made change in demo

--- vite-project/src/Pages/AddtoCart.jsx
+++ vite-project/src/Pages/AddtoCart.jsx
@@ -11,6 +11,7 @@ const AddtoCart = () => {
 
   return (
     <div className="w-full min-h-screen bg-gray-100 p-6">
+      <h1>changes</h1>
       <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-6">
         {/* Cart Items */}
         <div className="lg:col-span-2 space-y-4">
