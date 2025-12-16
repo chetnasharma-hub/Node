b51e149ff7d7a3d8c5a1cc1122f79fab05a4079e
chetnasharma-hub <chetnasharma162004@gmail.com>
2025-12-16 14:46:06 +0530

    ds

--- vite-project/src/Pages/Demo.jsx
+++ vite-project/src/Pages/Demo.jsx
@@ -35,7 +35,7 @@ const Home = () => {
 
 
     <div className="w-full  flex flex-col items-center justify-center bg-gray-200">
-   <p>Changesssss</p>
+   <p>Changesssss</p><h3>yaa</h3>
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-8 m-4">
         {/* Second Cards Starts*/}
         {items.map((item, index) => (
